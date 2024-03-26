d384de1d366509826909d0a8934f840f5f2831d1
Brandon Corey <bcorey3660@gmail.com>
2024-03-25 20:39:12 -0400

    add daisyui component library to application

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+
+🌼   daisyUI 4.8.0
+├─ ✔︎ 2 themes added		https://daisyui.com/docs/themes
+╰─ ★ Star daisyUI on GitHub	https://github.com/saadeghi/daisyui
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("daisyui")],
+};
