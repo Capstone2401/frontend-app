4d8ca234e2026436b5f45018437dbc5921d35b1b
Brandon Corey <bcorey3660@gmail.com>
2024-03-26 18:39:01 -0400

    decouple dropdown display values from internal values sent to server
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,25 +1,37 @@
-export default function Dropdown({ text, items, handleSetSelection }) {
+import { useRef } from "react";
+
+export default function Dropdown({
+  defaultText,
+  items,
+  selection,
+  handleSetSelection,
+}) {
   if (!items) {
     return null;
   }
+
+  const dropDownRef = useRef(null);
 
   return (
     <div className="dropdown dropdown-right">
       <div
         tabIndex={0}
         role="button"
-        className="btn w-52 text-base-100 bg-primary hover:bg-secondary"
+        className="btn w-52 text-neutral bg-primary hover:bg-secondary"
       >
-        {text}
+        {selection.title || defaultText}
       </div>
       <ul
         tabIndex={0}
         className="dropdown-content z-[1] menu p-2 shadow bg-neutral-600 text-white rounded-md w-52"
-        onClick={(e) => handleSetSelection(e)}
+        ref={dropDownRef}
       >
         {items.map((item) => (
-          <li key={item}>
-            <a>{item}</a>
+          <li
+            key={item.aggregation || item.title}
+            onClick={() => handleSetSelection(item, dropDownRef)}
+          >
+            <a data-aggregation={item.aggregation}>{item.title}</a>
           </li>
         ))}
       </ul>
