b46b9a104a4ebd9d2d5b48f9b3bbbe59a838cef7
Brandon Corey <bcorey3660@gmail.com>
2024-03-26 02:38:25 -0400

    add filter barebones filter dropdown component
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,13 @@
 import Dropdown from "./Dropdown";
 
 export default function Filter() {
+  const mockData = {
+    device: ["desktop", "iPhone", "Mac"],
+    city: ["Boston", "New york"],
+  };
   return (
-    <Dropdown />
-  )
+    <div className="p-5 w-full">
+      <Dropdown items={mockData.device} text={"Filter"} />
+    </div>
+  );
 }
