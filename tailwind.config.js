73a34c35dc02b4cb82c91862e17cf69b1b03be65
Brandon Corey <bcorey3660@gmail.com>
2024-03-26 02:41:17 -0400

    update configuration of project to enable for daisy ui theming
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,22 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#F1D492",
+          secondary: "#D68A2E",
+          accent: "#9D5517",
+          neutral: "#07040c",
+          "base-100": "#3C3838",
+          info: "#93c5fd",
+          success: "#c084fc",
+          warning: "#c88400",
+          error: "#ff92a6",
+        },
+      },
+    ],
+  },
 };
