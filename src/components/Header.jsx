c8b543393cc4683af139bbf2430a27d2c4b72157
Brandon Corey <bcorey3660@gmail.com>
2024-04-21 19:36:18 -0400

    update image paths to work in production

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,13 @@
+import logoText from "../assets/logo-text.png";
+import logoImage from "../assets/logo-image.png";
+
 export default function Header() {
   return (
     <div>
       <header className="border-b border-b-neutral-600 pb-6 flex justify-between">
         <div className="flex">
-          <img
-            src="./src/assets/logo-image.png"
-            className="h-12 self-center blur-xs"
-          />
-          <img
-            src="./src/assets/logo-text.png"
-            className="h-6 self-center blur-xs"
-          />
+          <img src={logoImage} className="h-12 self-center blur-xs" />
+          <img src={logoText} className="h-6 self-center blur-xs" />
         </div>
         <p className="text-right self-end">Powered by AWS</p>
       </header>
